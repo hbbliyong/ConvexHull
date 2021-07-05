812b031bb0512b962351cbae4ab8150aee9fc5fa
liyong <liyong@interhouse.com>
2021-07-05 09:22:35 +0800

    计算最小外接矩形

---
+++ ConvexHull.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvexHull
+{
+    public class ConvexHull
+    {
+
+        private RectData GetClosestRectange(List<XYZ> allPoint)
+        {
+            if (allPoint.Count < 3)
+            {
+                // MessageBox.Show("至少三个点");
+                return null;
+            }
+            else if (allPoint.Count == 3)
+            {
+                return drawList(allPoint);
+            }
+            else
+            {
+                //找最小凸包
+                List<XYZ> res = calcConvexHull(allPoint);
+                //凸包最小外接矩形
+                return drawList(res);
+            }
+        }
+        private List<XYZ> calcConvexHull(List<XYZ> list)
+        {
+            List<XYZ> resPoint = new List<XYZ>();
+            int minIndex = 0;
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].Y < list[minIndex].Y)
+                {
+                    minIndex = i;
+                }
+            }
+            XYZ minPoint = list[minIndex];
+            resPoint.Add(list[minIndex]);
+            list.RemoveAt(minIndex);
+            list.Sort(
+                delegate (XYZ p1, XYZ p2)
+                {
+                    vector baseVec;
+                    baseVec.x = 1;
+                    baseVec.y = 0;
+
+                    vector p1Vec;
+                    p1Vec.x = p1.X - minPoint.X;
+                    p1Vec.y = p1.Y - minPoint.Y;
+
+                    vector p2Vec;
+                    p2Vec.x = p2.X - minPoint.X;
+                    p2Vec.y = p2.Y - minPoint.Y;
+
+                    double up1 = p1Vec.x * baseVec.x;
+                    double down1 = Math.Sqrt(p1Vec.x * p1Vec.x + p1Vec.y * p1Vec.y);
+
+                    double up2 = p2Vec.x * baseVec.x;
+                    double down2 = Math.Sqrt(p2Vec.x * p2Vec.x + p2Vec.y * p2Vec.y);
+
+
+                    double cosP1 = up1 / down1;
+                    double cosP2 = up2 / down2;
+
+                    if (cosP1 > cosP2)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                );
+            resPoint.Add(list[0]);
+            resPoint.Add(list[1]);
+            for (int i = 2; i < list.Count; i++)
+            {
+                var basePt = resPoint[resPoint.Count - 2];
+                vector v1;
+                v1.x = list[i - 1].X - basePt.X;
+                v1.y = list[i - 1].Y - basePt.Y;
+
+                vector v2;
+                v2.x = list[i].X - basePt.X;
+                v2.y = list[i].Y - basePt.Y;
+
+                if (v1.x * v2.y - v1.y * v2.x < 0)
+                {
+                    resPoint.RemoveAt(resPoint.Count - 1);
+                    while (true)
+                    {
+                        var basePt2 = resPoint[resPoint.Count - 2];
+                        vector v12;
+                        v12.x = resPoint[resPoint.Count - 1].X - basePt2.X;
+                        v12.y = resPoint[resPoint.Count - 1].Y - basePt2.Y;
+                        vector v22;
+                        v22.x = list[i].X - basePt2.X;
+                        v22.y = list[i].Y - basePt2.Y;
+                        if (v12.x * v22.y - v12.y * v22.x < 0)
+                        {
+                            resPoint.RemoveAt(resPoint.Count - 1);
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+                    resPoint.Add(list[i]);
+                }
+                else
+                {
+                    resPoint.Add(list[i]);
+                }
+            }
+            return resPoint;
+        }
+        private RectData drawList(List<XYZ> list)
+        {
+            if (list == null || list.Count < 2)
+            {
+                return null;
+            }
+
+
+            var rects = GetAllNewRectDatas(list);
+            var rect = GetMinAreaRect(rects);
+
+            return rect;
+            if (list != null && list.Count > 1)
+            {
+                int cnt = list.Count;
+                for (int i = 0; i < cnt - 1; i++)
+                {
+                    //  graphic.DrawLine(pen, list[i], list[i + 1]);
+                }
+                //  graphic.DrawLine(pen, list[cnt - 1], list[0]);
+            }
+            if (list != null && list.Count > 1)
+            {
+                int cnt = rect.boundary.Count;
+                var xyzs = rect.boundary;
+                for (int i = 0; i < cnt - 1; i++)
+                {
+                    // graphic.DrawLine(pen1, XyzToPoint(xyzs[i]), XyzToPoint(xyzs[i + 1]));
+                }
+                //  graphic.DrawLine(pen1, XyzToPoint(xyzs[cnt - 1]), XyzToPoint(xyzs[0]));
+            }
+        }
+
+        // 1.寻找多边形的中心 
+        public XYZ GetCenter(List<XYZ> pts)
+        {
+            double sumx = 0;
+            double sumy = 0;
+            foreach (var p in pts)
+            {
+                sumx = sumx + p.X;
+                sumy = sumy + p.Y;
+            }
+            var pt = new XYZ(sumx / pts.Count(), sumy / pts.Count(), 0);
+            return pt;
+        }
+
+        // 2.旋转多边形，针对每个点实现绕中心点旋转
+
+        public XYZ RotatePt(XYZ inpt, XYZ centerPt, double theta)
+        {
+            double ix = inpt.X;
+            double iy = inpt.Y;
+            double cx = centerPt.X;
+            double cy = centerPt.Y;
+            double Q = theta / 180 * 3.1415926;  //角度
+
+            double ox, oy;
+            ox = (ix - cx) * Math.Cos(Q) - (iy - cy) * Math.Sin(Q) + cx;   //旋转公式
+            oy = (ix - cx) * Math.Sin(Q) + (iy - cy) * Math.Cos(Q) + cy;
+
+            var outpt = new XYZ(ox, oy, 0);
+            return outpt;
+        }
+
+        // 3.多边形旋转后求简单外接矩形
+
+        public List<XYZ> GetRect(List<XYZ> inpts)
+        {
+            var outpts = new List<XYZ>();
+            int size = inpts.Count();
+            if (size == 0)
+                return null;
+            else
+            {
+                var tempx = new List<double>();
+                var tempy = new List<double>();
+                for (int i = 0; i < size; i++)
+                {
+                    tempx.Add(inpts[i].X);
+                    tempy.Add(inpts[i].Y);
+                }
+
+                XYZ endpoint0 = new XYZ(tempx.Min(), tempy.Max(), 0);
+                XYZ endpoint1 = new XYZ(tempx.Max(), tempy.Max(), 0);
+                XYZ endpoint2 = new XYZ(tempx.Max(), tempy.Min(), 0);
+                XYZ endpoint3 = new XYZ(tempx.Min(), tempy.Min(), 0);
+                outpts.Add(endpoint0);
+                outpts.Add(endpoint1);
+                outpts.Add(endpoint2);
+                outpts.Add(endpoint3);
+                return outpts;
+            }
+        }
+        // 4.存储每个旋转角度下多边形的外接矩形，记录外接矩形的顶点坐标、面积和此时多边形的旋转角度
+
+        public class RectData
+        {
+            public List<XYZ> boundary { get; set; }
+            public XYZ center { get; set; }
+            public double theta { get; set; }
+            public double area { get; set; }
+
+        }
+
+        public RectData GetRotateRectDatas(List<XYZ> inpts, double theta)
+        {
+
+            XYZ center = GetCenter(inpts);
+            var tempvertices = new List<XYZ>();
+            for (int i = 0; i < inpts.Count(); i++)
+            {
+                XYZ temp = RotatePt(inpts[i], center, theta);
+                tempvertices.Add(temp);
+            }
+            List<XYZ> vertices = GetRect(tempvertices);
+            double deltaX, deltaY;                     //求每个外接矩形的面积
+            deltaX = vertices[0].X - vertices[2].X;
+            deltaY = vertices[0].Y - vertices[2].Y;
+
+            var polygen = new RectData
+            {
+                area = Math.Abs(deltaY * deltaX),
+                center = center,
+                theta = theta,
+                boundary = vertices
+            };
+            return polygen;
+        }
+
+        //获取所有新的矩形
+        public List<RectData> GetAllNewRectDatas(List<XYZ> inpts)
+        {
+            var polygens = new List<RectData>();
+
+            for (int theta = 0; theta <= 90;)
+            {
+                polygens.Add(GetRotateRectDatas(inpts, theta));
+                theta = theta + 5;
+            }
+            return polygens;
+        }
+        //获取新的矩形
+        public RectData GetMinAreaRect(List<RectData> polygons)
+        {
+
+            double minarea = 100000000;
+            int N = 0;
+            for (int i = 0; i < polygons.Count(); i++)
+            {
+                if (minarea > polygons[i].area)
+                {
+                    minarea = polygons[i].area;
+                    N = i;
+                }
+            }
+            var polygon = new RectData();
+            polygon = polygons[N];
+
+            //旋转到最小面积的方向
+            XYZ centerPt = GetCenter(polygon.boundary);
+            var boundary = new List<XYZ>();
+            foreach (var bound in polygon.boundary)
+            {
+                XYZ pt = RotatePt(bound, polygon.center, -polygon.theta);
+                boundary.Add(pt);
+            }
+            var outpolygon = new RectData
+            {
+                center = polygon.center,
+                area = polygon.area,
+                theta = polygon.theta,
+                boundary = boundary
+            };
+            return outpolygon;
+        }
+
+        public class XYZ
+        {
+
+            public XYZ(double v1, double v2, int v3)
+            {
+                this.X = v1;
+                this.Y = v2;
+                this.Z = v3;
+            }
+
+            public double X { get; set; }
+            public double Y { get; set; }
+            public double Z { get; set; }
+        }
+    }
+}
+
---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ConvexHull
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.drawPane = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.drawPane);
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.splitContainer1.Panel2.Controls.Add(this.button4);
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Size = new System.Drawing.Size(640, 454);
+            this.splitContainer1.SplitterDistance = 425;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // drawPane
+            // 
+            this.drawPane.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drawPane.Location = new System.Drawing.Point(0, 0);
+            this.drawPane.Name = "drawPane";
+            this.drawPane.Size = new System.Drawing.Size(425, 454);
+            this.drawPane.TabIndex = 0;
+            this.drawPane.MouseClick += new System.Windows.Forms.MouseEventHandler(this.drawPane_MouseClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(44, 191);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 22);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "清除点";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(44, 146);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 22);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "计算凸包";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 22);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "停止取点";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选取点";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(640, 454);
+            this.Controls.Add(this.splitContainer1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "散点凸包";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel drawPane;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
